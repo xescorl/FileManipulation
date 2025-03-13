e2ef761f7a8dfdf38cda981e79e445275dba0db8
Francesc Tur <francescturcla@gmail.com>
2025-03-13 17:11:21 +0100

    added the .py extension to the code and made a code that automatically adds the .py extension if a file is a .txt or doesn't have one

---
+++ ExtensionFixer.py
@@ -0,0 +1,16 @@
+import os
+
+def change_extension(folder_path, old_ext, new_ext):
+    for filename in os.listdir(folder_path):
+        if filename.endswith(old_ext):
+            base = os.path.splitext(filename)[0]
+            new_filename = base + new_ext
+            print(f'Changing {filename} to {new_filename}')
+            os.rename(os.path.join(folder_path, filename), os.path.join(folder_path, new_filename))
+        elif not os.path.splitext(filename)[1]:  # Check if there is no extension
+            new_filename = filename + new_ext
+            print(f'Adding extension to {filename} to make it {new_filename}')
+            os.rename(os.path.join(folder_path, filename), os.path.join(folder_path, new_filename))
+
+folder_path = r'C:\Users\ftur.DOMINIOEXPO\Documents\SW\pythonOfficeWordManipulation'
+change_extension(folder_path, '.txt', '.py')
